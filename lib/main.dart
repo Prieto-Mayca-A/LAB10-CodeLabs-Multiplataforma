e012a2cba63d430e1ead3e92810c943c21bd78d4
Prieto-Mayca-A <68166806+Prieto-Mayca-A@users.noreply.github.com>
2021-05-21 13:28:47 -0500

    modelo 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+//import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -127,4 +147,6 @@ void _pushSaved() {
       ),
     );
   }
+
 }
+*/
